7ae20c268a085c935ff5935f4b73d2fbee1f5d32
katief01 <74223626+katief01@users.noreply.github.com>
2021-03-29 15:34:26 -0500

    Updated RScript file Deliverable 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -18,3 +18,11 @@ total_summary <- SCoil_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median
 
 # Create a lot_summary dataframe 
 lot_summary <- SCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), PSI_Variance=var(PSI), Standard_Devation=sd(PSI), .groups = 'keep')
+
+# Determine if PSI across all manufacturing lots is statistically diff from the pop. mean of 1,500 lbs per sq inch.
+t.test(SCoil_table$PSI,mu = 1500)
+
+# Write 3 more RScripts to determine if PSI for each manufacturing lot is statistically diff from the pop mean of 1,500 lbs per sq inch.
+t.test(lot_summary$Manufacturing_Lot=="Lot1",mu = 1500)
+t.test(lot_summary$Manufacturing_Lot=="Lot2",mu = 1500)
+t.test(lot_summary$Manufacturing_Lot=="Lot3",mu = 1500)
