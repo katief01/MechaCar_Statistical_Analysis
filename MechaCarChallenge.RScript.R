41c07eef03cad1915e4639ee663cb586c4dd9cef
katief01 <74223626+katief01@users.noreply.github.com>
2021-03-29 11:46:24 -0500

    Updated RScript file - Deliverable #2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -9,3 +9,12 @@ multi_lr <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cl
 
 # Determine the p-value and the r-squared value for the linear regression model
 summary(multi_lr)
+
+# import and read in the Suspension_Coil.csv file as a table.
+SCoil_table <- read.csv(file="Suspension_Coil.csv")
+
+# Create a total_summary dataframe using the summarize() function on PSI column
+total_summary <- SCoil_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), PSI_Variance=var(PSI), Standard_Devation=sd(PSI), .groups = 'keep')
+
+# Create a lot_summary dataframe 
+lot_summary <- SCoil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), PSI_Variance=var(PSI), Standard_Devation=sd(PSI), .groups = 'keep')
